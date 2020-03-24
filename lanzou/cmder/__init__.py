95c588c274394ffcb655fe5618b8c0c7685ca5da
zaxtyson <zaxtyson@qq.com>
2020-03-24 17:59:00 +0800

    add reader mode

--- lanzou/cmder/__init__.py
+++ lanzou/cmder/__init__.py
@@ -1,5 +1,5 @@
 from lanzou.cmder.config import config
 
-version = '2.4.0'
+version = '2.4.1'
 
-__all__ = ['cmder', 'utils', 'update', 'version', 'config']
+__all__ = ['cmder', 'utils', 'version', 'config']
